27d8c17f2fd2224c9aae8ea68b6223906a29ef0b
INGCAG <ingcaguayaral@gmail.com>
2019-07-01 23:49:47 +0200

    Sensor scheme adjust for JPA

--- src/main/java/com/model/Sensor.java
+++ src/main/java/com/model/Sensor.java
@@ -1,22 +1,29 @@
 package com.model;
 
 import org.springframework.stereotype.Component;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Entity
 @Component
 public class Sensor {
 
+    @Id
     private Long sensorId;
 
     private Double temperature;
 
-    public Sensor(){
+    public Sensor(){}
 
-        this.sensorId = 1L;
-        this.temperature = 25.0;
+    public Sensor(Long sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    public Sensor(Double temperature){ this.temperature = temperature; }
+
+    public Sensor(Long sensorId, Double temperature){
+        this.sensorId = sensorId;//1L
+        this.temperature = temperature;//25.0;
     }
 
     public Long getSensorId() {
--- src/main/java/com/repository/Sensors.java
+++ src/main/java/com/repository/Sensors.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called devicesRepository
 // CRUD refers Create, Read, Update, Delete
 public interface Sensors extends CrudRepository<Sensor, Long> {
-    Optional<Sensor> findById(Long deviceId);
+    Optional<Sensor> findById(Long sensorId);
 }
